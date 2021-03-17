574ac6f695bffe1a87dfeca9c6f2dafb4b068af4
May Guo <40670609+windcloak@users.noreply.github.com>
2021-03-16 22:30:45 -0500

    Added UI prompt to pick up item

---
+++ Assets/FPS/Scripts/PickupHUD.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PickupHUD : MonoBehaviour
+{
+
+    public GameObject PickupPanel;
+
+    public void OpenPickupPanel()
+    {
+        PickupPanel.SetActive(true);
+    }
+
+    public void ClosePickupPanel()
+    {
+        PickupPanel.SetActive(false);
+    }
+}
---
+++ Assets/FPS/Scripts/PickupItem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PickupItem : MonoBehaviour
+{
+    public PickupHUD pickup;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        // Todo: check if item is an inventory item
+        pickup.OpenPickupPanel();
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        pickup.ClosePickupPanel();
+    }
+}

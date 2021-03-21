fa38cf65931c918e55ab5b0c8f5894c0c4aa6fdb
May Guo <40670609+windcloak@users.noreply.github.com>
2021-03-20 21:56:16 -0500

    Added Money (UI) and Coin script on placeholder coin

---
+++ Assets/Coin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    // public GameObject eatenEffect;
+    public float coinAmount = 10f;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            PickupCoin();
+        }
+
+    }
+
+    void PickupCoin()
+    {
+        // FindObjectOfType<AudioManager>().Play("Coin");
+        Money.UpdateMoney(coinAmount);
+        // Instantiate(eatenEffect, transform.position, transform.rotation);   // show effect
+        Destroy(gameObject);    // destroy coin
+    }
+}
---
+++ Assets/Money.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Money : MonoBehaviour
+{
+    public TMP_Text scoreText;
+    public static float money = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = money.ToString("0");
+    }
+
+    // increase (find loot) or decrease money (buy stuff)
+    public static void UpdateMoney(float n)
+    {
+        money += n;
+    }
+
+    // reset on game over
+    public static void ResetMoney()
+    {
+        money = 0;
+    }
+
+    public static float GetMoney()
+    {
+        return money;
+    }
+}

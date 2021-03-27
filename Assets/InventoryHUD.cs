d5469d9a2e54f72496a98b6e5c1b059b4acc58d6
May Guo <40670609+windcloak@users.noreply.github.com>
2021-03-26 23:42:16 -0500

    disable camera movement when inventory is open

--- Assets/InventoryHUD.cs
+++ Assets/InventoryHUD.cs
@@ -4,6 +4,8 @@ public class InventoryHUD : MonoBehaviour
 {
     public GameObject inventoryPanel;
     public static bool isInventoryOpen = false;
+    public GameObject menuRoot;
+    PlayerInputHandler m_PlayerInputsHandler;
 
     // Update is called once per frame
     void Update()
@@ -14,12 +16,10 @@ public class InventoryHUD : MonoBehaviour
             if (!isInventoryOpen)   
             {
                 OpenInventoryPanel();
-                isInventoryOpen = true;
             }
             else
             {
                 CloseInventoryPanel();
-                isInventoryOpen = false;
             }
         }
     }
@@ -27,11 +27,32 @@ public class InventoryHUD : MonoBehaviour
 
     public void OpenInventoryPanel()
     {
+        isInventoryOpen = true;
         inventoryPanel.SetActive(true);
+
+        if (!menuRoot.activeSelf && Input.GetMouseButtonDown(0))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        // disable in-game mouse movement
+        Cursor.lockState = CursorLockMode.None;
+    
+        Cursor.visible = true;
+        // TODO need to disable clicking/shooting in-game
+        Time.timeScale = 0f;
+
+
     }
 
     public void CloseInventoryPanel()
     {
+        isInventoryOpen = false;
         inventoryPanel.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1f;
     }
 }

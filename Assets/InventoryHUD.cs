c7a43453802b22687787776730f4a70bc79e651f
May Guo <40670609+windcloak@users.noreply.github.com>
2021-03-26 22:28:50 -0500

    Press I key to open Inventory

---
+++ Assets/InventoryHUD.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class InventoryHUD : MonoBehaviour
+{
+    public GameObject inventoryPanel;
+    public static bool isInventoryOpen = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            // open panel if it's not already open
+            if (!isInventoryOpen)   
+            {
+                OpenInventoryPanel();
+                isInventoryOpen = true;
+            }
+            else
+            {
+                CloseInventoryPanel();
+                isInventoryOpen = false;
+            }
+        }
+    }
+
+
+    public void OpenInventoryPanel()
+    {
+        inventoryPanel.SetActive(true);
+    }
+
+    public void CloseInventoryPanel()
+    {
+        inventoryPanel.SetActive(false);
+    }
+}

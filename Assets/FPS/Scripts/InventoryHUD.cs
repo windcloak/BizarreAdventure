7b3d39e282f991ba534a725068851e8ea565b680
May Guo <40670609+windcloak@users.noreply.github.com>
2021-03-28 20:26:43 -0500

    added remove functionality

--- Assets/FPS/Scripts/InventoryHUD.cs
+++ Assets/FPS/Scripts/InventoryHUD.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InventoryHUD : MonoBehaviour
 {
-    public GameObject inventoryPanel;
-    public static bool isInventoryOpen = false;
     public GameObject menuRoot;
+    public static bool isInventoryOpen;
     PlayerInputHandler m_PlayerInputsHandler;
 
     // Update is called once per frame
     void Update()
     {
+        if (!menuRoot.activeSelf && Input.GetMouseButtonDown(0))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        // press I to pull up inventory
         if (Input.GetKeyDown(KeyCode.I))
         {
             // open panel if it's not already open
@@ -22,37 +29,60 @@ public class InventoryHUD : MonoBehaviour
                 CloseInventoryPanel();
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.I)
+    || (menuRoot.activeSelf && Input.GetButtonDown(GameConstants.k_ButtonNameCancel)))
+        {
+            SetPauseMenuActivation(!menuRoot.activeSelf);
+
+        }
+
+        if (Input.GetAxisRaw(GameConstants.k_AxisNameVertical) != 0)
+        {
+            if (EventSystem.current.currentSelectedGameObject == null)
+            {
+                EventSystem.current.SetSelectedGameObject(null);
+            }
+        }
+
     }
 
+    private void Start()
+    {
+        m_PlayerInputsHandler = FindObjectOfType<PlayerInputHandler>();
+        DebugUtility.HandleErrorIfNullFindObject<PlayerInputHandler, InGameMenuManager>(m_PlayerInputsHandler, this);
+        menuRoot.SetActive(false);
+    }
+
+    void SetPauseMenuActivation(bool active)
+    {
+        menuRoot.SetActive(active);
+
+        if (menuRoot.activeSelf)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Time.timeScale = 0f;
+            EventSystem.current.SetSelectedGameObject(null);
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            Time.timeScale = 1f;
+        }
+
+    }
 
     public void OpenInventoryPanel()
     {
         isInventoryOpen = true;
-        inventoryPanel.SetActive(true);
-
-        if (!menuRoot.activeSelf && Input.GetMouseButtonDown(0))
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-        }
-
-        // disable in-game mouse movement
-        Cursor.lockState = CursorLockMode.None;
-    
-        Cursor.visible = true;
-        // TODO need to disable clicking/shooting in-game
-        Time.timeScale = 0f;
-
-
+        menuRoot.SetActive(true);
     }
 
     public void CloseInventoryPanel()
     {
         isInventoryOpen = false;
-        inventoryPanel.SetActive(false);
-
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-        Time.timeScale = 1f;
+        menuRoot.SetActive(false);
     }
 }
--- Assets/FPS/Scripts/UI/InventorySlot.cs
+++ Assets/FPS/Scripts/UI/InventorySlot.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 public class InventorySlot : MonoBehaviour
 {
     public Image icon;
-
+    public Button removeButton;
     Item item; //current item in the slot
 
     public void AddItem (Item newItem)
@@ -12,6 +12,7 @@ public class InventorySlot : MonoBehaviour
         item = newItem;
         icon.sprite = item.icon;
         icon.enabled = true;
+        removeButton.interactable = true;
         Debug.Log("Added item");
     }
     public void ClearSlot()
@@ -20,5 +21,18 @@ public class InventorySlot : MonoBehaviour
         icon.sprite = null;
         icon.enabled = false;
         Debug.Log("Cleared item");
+        removeButton.interactable = false;
+    }
+
+    public void OnRemoveButton()
+    {
+        Inventory.instance.Remove(item);
+        Debug.Log("item removed");
+    }
+
+    public void UseItem()
+    {
+        // customize for item
+        Debug.Log("item is being used");
     }
 }

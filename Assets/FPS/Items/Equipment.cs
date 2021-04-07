0c292c6d727b26af4f179a074f4680c42f2fcd7d
May Guo <40670609+windcloak@users.noreply.github.com>
2021-04-07 00:49:53 -0500

    Moved inventory panel on screen

---
+++ Assets/FPS/Items/Equipment.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
+public class Equipment : Item
+{
+    public int armorModifier;
+    public int hp;  // potion
+
+    public override void Use()
+    {
+        base.Use();
+        // Equip the item
+    }
+}
+
+public enum EquipmentSlot {  Armor, Helmet, Potion, Gun }

b5983c36466d2fbd7384c99291f6c38a66bb339c
May Guo <40670609+windcloak@users.noreply.github.com>
2021-04-15 22:10:34 -0500

    fixed obsolete error from import

--- Assets/52SpecialEffectPack/Animation&Script/csShowAllEffect.cs
+++ Assets/52SpecialEffectPack/Animation&Script/csShowAllEffect.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class csShowAllEffect : MonoBehaviour
 {
     public string[] EffectName;
     public Transform[] Effect;
-    public GUIText Text1;
+    public Text Text1;
     public int i = 0;
 
     void Start()

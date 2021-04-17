fb8a7bc48f200e47667906a5e65caf24e47cfef0
May Guo <40670609+windcloak@users.noreply.github.com>
2021-04-16 23:28:01 -0500

    Make pickup items bob and rotate

--- Assets/FPS/Scripts/ItemPickup.cs
+++ Assets/FPS/Scripts/ItemPickup.cs
@@ -8,8 +8,42 @@ public class ItemPickup : Interactable
     public GameObject pickupVFX;
     [Tooltip("Sound played on pickup")]
     public AudioClip pickupSFX;
+
+    [Tooltip("Frequency at which the item will move up and down")]
+    public float verticalBobFrequency = 1f;
+    [Tooltip("Distance the item will move up and down")]
+    public float bobbingAmount = 1f;
+    [Tooltip("Rotation angle per second")]
+    public float rotatingSpeed = 360f;
+    [Tooltip("Direction it rotates")]
+    public RotateDirection rotateDirection;
+
+    public Rigidbody pickupRigidbody { get; private set; }
+    Collider m_Collider;
+    Vector3 m_StartPosition;
     Pickup m_Pickup;
     bool m_HasPlayedFeedback;
+
+    public enum RotateDirection { up, forward, right }  // there are more, but I think we just need these
+
+    private void Update()
+    {
+        // Handle bobbing
+        float bobbingAnimationPhase = ((Mathf.Sin(Time.time * verticalBobFrequency) * 0.5f) + 0.5f) * bobbingAmount;
+        transform.position = m_StartPosition + Vector3.up * bobbingAnimationPhase;
+
+        RotateItem();
+
+    }
+
+    private void Start()
+    {
+        // Remember start position for animation
+        m_StartPosition = transform.position;
+        // ensure the physics setup is a kinematic rigidbody trigger
+        pickupRigidbody.isKinematic = true;
+        m_Collider.isTrigger = true;
+    }
 
     public override void Interact()
     {
@@ -46,5 +80,22 @@ public class ItemPickup : Interactable
         }
 
         m_HasPlayedFeedback = true;
+    }
+
+    // Handle rotate direction of item
+    private void RotateItem()
+    {
+        switch (rotateDirection)
+        {
+            case RotateDirection.up:
+                transform.Rotate(Vector3.up, rotatingSpeed * Time.deltaTime, Space.Self);
+                break;
+            case RotateDirection.forward:
+                transform.Rotate(Vector3.forward, rotatingSpeed * Time.deltaTime, Space.Self);
+                break;
+            case RotateDirection.right:
+                transform.Rotate(Vector3.right, rotatingSpeed * Time.deltaTime, Space.Self);
+                break;
+        }
     }
 }
